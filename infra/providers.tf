251150da18fa23d3f3cb7cb04deb5c268f818402
Jimpitos <lucasmiyoshi81@gmail.com>
2026-08-21 00:25:11 +0000

    feat: implementa infraestrutura terraform, azure function e pipelines ci/cd

---
+++ infra/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-20082026"
+    storage_account_name = "sttfstatequeimadas555295"
+    container_name       = "tfstate"
+    key                  = "monitor-queimadas.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
